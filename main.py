8b684551496a1d53bda958c3f2eae269b3bb477b
vitaly <orangebester@gmail.com>
2021-03-18 14:42:11 +0200

    Added docstrings and comments

--- main.py
+++ main.py
@@ -25,16 +25,22 @@ worksheet_name = now.strftime("%B %Y")
 
 
 def tasks_json():
+    """Creates json file and downloads there data about tasks and subtasks in Asana"""
+
     output_list = []
     output_list_subtasks = []
     count = 0
     project_gid = ['1198913045061016', '1198913045061019',
                    '1198913121709013', '1198913121709020', '1198913121709027', '1198913121709034', '1198913121709041', '1199104176594285']
     # pylint: disable=maybe-no-member
+
+    # download tasks
     for i in project_gid:
         result = client.tasks.get_tasks_for_project(i, {'completed_since': f'{now.year}-{now.month:02d}-01T02:00:00.000Z'}, opt_pretty=True, opt_fields=[
             'name', 'assignee.name', 'completed', 'completed_at',  'tags.name', 'subtasks'])
         output_list += list(result)
+
+    # download subtasks
     for i in output_list:
         if len(i['subtasks']) != 0:
             result = client.tasks.get_subtasks_for_task(i['gid'], {'completed_since': f'{now.year}-{now.month:02d}-01T02:00:00.000Z'}, opt_pretty=True, opt_fields=[
@@ -43,12 +49,20 @@ def tasks_json():
             count += 1
             if count % 149 == 0:
                 time.sleep(60.0)
+
     output_list.extend(output_list_subtasks)
     with open("meta.json", "w", encoding="utf-8") as output:
         json.dump(output_list, output, ensure_ascii=False)
 
 
 def cell_updater(cell_range_insert, value_range_body):
+    """Updates data in cells
+
+    Args: 
+        cell_range_insert (str): starting cell range
+        value_range_body (dict): values to download
+    """
+
     # pylint: disable=maybe-no-member
     time.sleep(1.1)
     service.spreadsheets().values().update(
@@ -59,6 +73,13 @@ def cell_updater(cell_range_insert, value_range_body):
 
 
 def cell_appender(cell_range_insert, value_range_body):
+    """Appends data to other cells
+
+    Args:
+        cell_range_insert (str): starting point cell
+        value_range_body (dict): values to download
+    """
+
     # pylint: disable=maybe-no-member
     service.spreadsheets().values().append(
         spreadsheetId=spreadsheet_id,
@@ -68,15 +89,19 @@ def cell_appender(cell_range_insert, value_range_body):
 
 
 def tasks_spreadsheet():
+    """Updates cells in table according to data in json file"""
+
     startrow_index = 1
     n = cell_days()
     alphabet = ['B', 'D', 'F', 'H', 'J', 'L',
                 'N', 'P', 'R', 'T', 'V', 'X', 'Z']
+    # downloading data from json
     with open("meta.json", 'r', encoding="utf-8") as f:
         data = json.load(f)
         data = [k for k in data if not (k['completed_at'] == None)]
         data = sorted(data, key=lambda k: k['completed_at'])
 
+    # downloading and formatting names of assignee
     names_list = []
     for i in range(len(data)):
         if data[i]['assignee'] is not None:
@@ -86,6 +111,8 @@ def tasks_spreadsheet():
     i = 0
     format_name(sheet_id, service, spreadsheet_id)
     format_scnd_header(sheet_id, service, spreadsheet_id)
+
+    # downloading tasks by names of assignee
     for a in names_list:
         values = (
             (f'{a}', ''),
@@ -96,11 +123,15 @@ def tasks_spreadsheet():
             'values': values
         }
         cell_updater(f"{alphabet[i]}1", value_range_body)
+
+        # formatting cells
         merge_columns(startrow_index, sheet_id, service, spreadsheet_id)
         format_bold(startrow_index, sheet_id, service, spreadsheet_id)
         format_tasks(startrow_index, sheet_id, service, spreadsheet_id)
         format_time_tasks(startrow_index, sheet_id, service, spreadsheet_id)
+
         startrow_index += 2
+        # sorting tasks by name and checking whether task is completed
         data_new = [k for k in data if (k['assignee'] is not None and k['assignee']
                                         ['name'] == a and k['completed_at'] is not None)]
         for d in range(len(data_new)):
@@ -112,7 +143,10 @@ def tasks_spreadsheet():
                 day = int(date[9])
             else:
                 day = int(date[8] + date[9])
+            # adding new tag "day"
             data_new[d]['day'] = day
+
+        # checking whether task has a tag
         data_new = [k for k in data_new if len(k['tags']) != 0]
         while len(data_new) >= 1:
             data_so_new = [k for k in data_new if (
@@ -120,11 +154,14 @@ def tasks_spreadsheet():
             data_new = [k for k in data_new if k not in data_so_new]
             var1 = float(data_so_new[0]['tags'][0]['name'].replace(',', '.'))
             var2 = f"{data_so_new[0]['tags'][0]['name'].replace('.',',')} - {data_so_new[0]['name']}\n"
+
             if len(data_so_new) > 1:
+                # updating task if there are more than one tag
                 for c in range(len(data_so_new[1:])):
                     var1 += float(data_so_new[1:][c]['tags']
                                   [0]['name'].replace(',', '.'))
                     var2 += f"{data_so_new[1:][c]['tags'][0]['name'].replace('.',',')} - {data_so_new[1:][c]['name']}\n"
+
             var1 = str(var1).replace('.', ',')
             values_tasks = (
                 (f'{var1}',
@@ -137,6 +174,8 @@ def tasks_spreadsheet():
             }
             cell_updater(f"{alphabet[i]}{data_so_new[0]['day']+2}",
                          value_range_body_tasks)
+
+        # summing values of all tasks for one assignee
         values = (
             (f'=СУММ({alphabet[i]}3:{alphabet[i]}{n-2})', ''),
             ('', '')
@@ -150,6 +189,8 @@ def tasks_spreadsheet():
 
 
 def float_tasks(a):
+    '''Checks whether type of tag is not str'''
+
     try:
         return float(a['name'].replace(',', '.'))
     except ValueError:
@@ -157,6 +198,12 @@ def float_tasks(a):
 
 
 def cell_days():
+    """Creates days in table referring to number of days of current month
+
+    Returns: 
+        k: number of days 
+    """
+
     # pylint: disable=maybe-no-member
     k = 28
     i = 31
@@ -169,8 +216,11 @@ def cell_days():
         'majorDimension': 'COLUMNS',
         'values': values_time
     }
+
     cell_updater(f'A3', value_range_body_time)
     q_days = monthrange(now.year, now.month)
+
+    # adding more days whether number of days of current month > 28
     while i <= (q_days[1]+2):
         k += 1
         values = (
@@ -183,6 +233,8 @@ def cell_days():
         }
         cell_updater(f'A{i}', value_range_body)
         i += 1
+
+    # adding "Сумма" in the of the list
     if i > (q_days[1] + 2):
         i = i-1
         values_sum = (
