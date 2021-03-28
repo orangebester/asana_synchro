8391e18d33f8c6ec09d9da97cd064f08b23abcfe
vitaly <orangebester@gmail.com>
2021-03-28 22:37:36 +0300

    description to personal_data.py added

--- personal_data.py
+++ personal_data.py
@@ -1,4 +1,3 @@
-personal_access_token = "1/1135096210698629:718f78c0f27970b442cd307018d87f8d"
-spreadsheet_id = '1LXz234UItKLiIccEgqjqwjkmvNfhcT2y_ni53AyEOOE'
-project_ids = ['1198913045061016', '1198913045061019', '1198913121709013', '1198913121709020',
-               '1198913121709027', '1198913121709034', '1198913121709041', '1199104176594285']
+personal_access_token = "..."  # write your personal Asana token
+spreadsheet_id = '...'  # write your spreadsheet ID
+project_ids = []  # write id`s of your Asana projects that need to be transferred
